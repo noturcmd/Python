7b3f0451e3ac742d793d49b7aef5fd00ef8330b9
noturcmd <ridwan170604bakhri@gmail.com>
2023-07-21 22:15:15 +0700

    push 1

---
+++ PrintAngka.py
@@ -0,0 +1,16 @@
+
+print("_-"*25)
+print("Perkalian".center(50, "_"))
+print("_-"*25)
+
+for i in range(1, 9):
+    for j in range(i, (i*i)+1, i):
+        print(j, end=" ")
+    print()
+
+
+
+
+
+
+
