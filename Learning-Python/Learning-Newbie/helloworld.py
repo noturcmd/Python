015fa01f4be758d9740fab625b957c559f5511f3
noturcmd <ridwan170604bakhri@gmail.com>
2023-07-21 22:31:18 +0700

    membuat file helloworld.py

---
+++ Learning-Python/Learning-Newbie/helloworld.py
@@ -0,0 +1,13 @@
+
+
+# Program Python pertama
+
+print("Hello, World!")
+print('Hello, World')
+
+[print(i, end="") for i in ["p".capitalize(), "y", "t", "h", "o", "n\n"]]
+[print(i.capitalize(), end="") for i in ["python"]]
+print()
+
+for i in "python":
+    print(i, end="")
